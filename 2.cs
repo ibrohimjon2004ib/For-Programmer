986889a627fda8ea8be47aa9757f10ab72b083df
unknown <i3344161@gmail.com>
2024-01-25 13:12:09 +0500

    Ҳами мисола ябор бин ку туғриҳай шартш болода ҳаст.

---
+++ 2.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int t = 0; t < 20; t++)
+            {
+                Console.Clear();
+
+                /* ƒано целое число N (> 0). ≈сли оно €вл€етс€ степенью числа 3, то
+                    вывести TRUE, если не €вл€етс€ Ч вывести FALSE. */
+                
+                //-------------- in side programm ------------------------//                                                                           
+                int n = int.Parse(Console.ReadLine());
+                //bool i = n == 3;
+                int j = 0;
+                while (j++ <= n)
+                {
+                    Console.WriteLine(i);                 
+                }
+                //------------ in side programm ------------------//
+
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
